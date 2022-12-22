4991bfa3d61525abd85d849a7832e9f219160e19
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-12-22 11:58:05 +0100

    Added theme component to app

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+    color: {
+        black: "#000",
+        white: "#fff",
+        teal: "#008080",
+        silver: "#cccccc",
+        gallery: "#eee",
+    },
+
+    breakpoint: {
+        mobileMax: 767,
+    }
+ };
