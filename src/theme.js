3444b46ffe81725e053bd86270cfee3c0a7391e1
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-12-22 20:13:14 +0100

    Added new colors to theme palette

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,9 @@ export const theme = {
         teal: "#008080",
         silver: "#cccccc",
         gallery: "#eee",
+        forest_green: "#1a7a1d",
+        pomegranate: "#ee2b2b",
+        alto: "#ddd",
     },
 
     breakpoint: {
