52a060d89546d2936ce94af16356b872266ffd14
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-13 16:21:38 +0100

    Eddited spaces

---
+++ src/features/tasks/getExampleTasks.js
@@ -0,0 +1,9 @@
+export const getExampleTasks = async () => {
+    const response = fetch("todo_list_react_v2/exampleTasks.json");
+
+    if(!response.ok) {
+        new Error (response.statusText);
+    }
+
+    return await response.json();
+};
