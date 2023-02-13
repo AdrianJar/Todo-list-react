d9a8ed1d2e0af56cd45c91df0c9e6f41401161c1
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-13 17:43:13 +0100

    small changes forgot await fetch

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,5 +1,5 @@
 export const getExampleTasks = async () => {
-    const response = fetch("/todo_list_react_v2/exampleTasks.json");
+    const response = await fetch("/todo_list_react_v2/exampleTasks.json");
 
     if (!response.ok) {
         new Error(response.statusText);
