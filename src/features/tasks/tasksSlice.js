e2f83d1c53f143221703340476bb71a71e170a41
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-13 23:51:13 +0100

    Added tasks selector

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -44,5 +44,6 @@ export const {
     setTasks,
 } = tasksSlice.actions;
 export const selectTasksState = state => state.tasks;
+export const selectTasks = (state) => selectTasksState(state).tasks;
 export default tasksSlice.reducer;
 
