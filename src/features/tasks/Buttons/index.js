b27d2b6dab4397d674c36c97f6f806509a86aa9e
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-13 16:22:24 +0100

    Added new button for example tasks

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,27 +1,37 @@
 import { StyledDiv, StyledButton } from "./styled";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasksState, setAllDone, toggleHideDone } from "../tasksSlice";
+import { selectTasksState, setAllDone, toggleHideDone, fetchExampleTasks } from "../tasksSlice";
 
 const Buttons = () => {
     const { tasks, hideDone } = useSelector(selectTasksState);
     const dispatch = useDispatch();
+    const areTasksEmpty = tasks.length > 0
+
     return (
-        tasks.length > 0 && (
-            <StyledDiv>
-                <StyledButton
-                    onClick={() => dispatch(toggleHideDone())}
-                >
-                    {hideDone ? "Pokaż " : "Ukryj "}ukończone
-                </StyledButton>
-                <StyledButton
-                    onClick={() => dispatch(setAllDone())}
-                    disabled={tasks.every(({ done }) => done)}
-                >
-                    Ukończ wszystkie
-                </StyledButton>
-            </StyledDiv>
-        )
+
+
+        <StyledDiv>
+            <StyledButton onClick={() => dispatch(fetchExampleTasks())}>
+                Pobierz przykładowe zadania
+            </StyledButton>
+            {areTasksEmpty && (
+                <>
+                    <StyledButton
+                        onClick={() => dispatch(toggleHideDone())}
+                    >
+                        {hideDone ? "Pokaż " : "Ukryj "}ukończone
+                    </StyledButton>
+                    <StyledButton
+                        onClick={() => dispatch(setAllDone())}
+                        disabled={tasks.every(({ done }) => done)}
+                    >
+                        Ukończ wszystkie
+                    </StyledButton>
+                </>
+            )}
+        </StyledDiv>
     )
+
 };
 
 export default Buttons;
