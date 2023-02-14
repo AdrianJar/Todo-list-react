bf8965c4a7386ff753a501027cda5311d8d5147d
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-14 23:40:51 +0100

    Added 2 links to site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Author from "./features/author/Author";
 import Tasks from "./features/tasks/Tasks";
 
 function App() {
-  
+
   return (
-    <Tasks />
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/zadania">Zadania</Link>
+          </li>
+          <li>
+            <Link to="/author">O autorze</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/zadania">
+            <Tasks />
+          </Route>
+          <Route path="/author">
+            <Author />
+          </Route>
+        </Switch>
+      </nav>
+    </BrowserRouter>
   )
 };
 
