e77682014dd03c27961b91d0f76c0d414a0ab823
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-15 22:26:19 +0100

    Added new component to app

---
+++ src/features/tasks/TasksPage/Search/index.js
@@ -0,0 +1,34 @@
+import { useHistory, useLocation } from "react-router-dom";
+import Input from "../../../Input";
+import searchQueryParamName from "../searchQueryParamName";
+import { InputDiv } from "./styled";
+
+const Search = () => {
+    const location = useLocation();
+    const history = useHistory();
+    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+
+    const onInputChange = ({ target }) => {
+        const searchParams = (new URLSearchParams(location.search))
+
+        if (target.value.trim() === "") {
+            searchParams.delete(searchQueryParamName);
+        } else {
+            searchParams.set(searchQueryParamName, target.value)
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+    };
+
+    return (
+        <InputDiv>
+            <Input
+                placeholder="Filtruj zadania:"
+                value={query || ""}
+                onChange={onInputChange}
+            />
+        </InputDiv>
+    );
+}
+
+export default Search;
---
+++ src/features/tasks/TasksPage/Search/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const InputDiv = styled.div`
+    padding: 20px;
+    display: grid;
+    grid-template-columns: 1fr;
+`;
