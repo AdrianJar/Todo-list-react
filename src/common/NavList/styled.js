1050bb34092cd7954f976e513a5042ac8b6d626c
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-16 23:55:50 +0100

    small changes

--- src/common/NavList/styled.js
+++ src/common/NavList/styled.js
@@ -2,25 +2,26 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const StyledList = styled.ul`
-background-color: ${({ theme }) => theme.color.teal};
-margin-top: 0px;
-padding: 20px;
-display: flex;
-flex-direction: row;
-justify-content: center;
-gap: 40px;
+    background-color: ${({ theme }) => theme.color.teal};
+    margin-top: 0px;
+    padding: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 40px;
 `;
 
 export const StyledItem = styled.li`
-list-style-type: none;
+    list-style-type: none;
 `;
 
 export const StyledNavLink = styled(NavLink)`
-text-decoration: none;
-color: ${({ theme }) => theme.color.white};
-font-size: 20px;
-&.active{
-    filter:brightness(120%);
-    font-weight: bold;
+    text-decoration: none;
+    color: ${({ theme }) => theme.color.white};
+    font-size: 20px;
+    
+    &.active{
+        filter:brightness(120%);
+        font-weight: bold;
 }
 `;
