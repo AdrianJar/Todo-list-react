0e4abb5f2eaed965106183485c5422340d937fe9
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-17 01:30:36 +0100

    moved navigation higher and implemeted components

---
+++ src/Navigation/index.js
@@ -0,0 +1,19 @@
+import { toAuthor, toTasks } from "../routes";
+import { StyledItem, StyledList, StyledNavLink } from "./styled";
+
+const Navigation = () => {
+
+  return (
+    <StyledList>
+      <StyledItem>
+        <StyledNavLink to={toTasks}>Zadania</StyledNavLink>
+      </StyledItem>
+      <StyledItem>
+        <StyledNavLink to={toAuthor}>O autorze</StyledNavLink>
+      </StyledItem>
+    </StyledList>
+  );
+
+}
+
+export default Navigation;
